44efb25dc7f5c725ec12dd6e7161c2eea703a0e7
Niteshpix <nitesh@pixlerlab.com>
2023-04-17 18:00:08 +0530

    chnages

--- src/Componets/posts.js
+++ src/Componets/posts.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { admin, client } from "../config/client";
+import { client } from "../config/client";
 import { Card, Col, Row } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { formatInTimeZone } from "date-fns-tz";
@@ -7,8 +7,6 @@ import { formatInTimeZone } from "date-fns-tz";
 function Posts() {
   const [articles, setArticles] = useState([]);
   const navigate = useNavigate();
-  const [author, setAuthor] = useState();
-  let space_id = process.env.REACT_APP_CONTENTFUL_SPACE_ID;
 
   const dateFormat = (date) => {
     return formatInTimeZone(date, "America/New_York", "dd MMM yyyy");
@@ -20,48 +18,18 @@ function Posts() {
         const response = await client.getEntries({
           content_type: "recipes",
         });
-
         setArticles(response.items);
       } catch (error) {
         console.log(error);
       }
     }
-    admin
-      .getSpace(space_id)
-      .then((space) => {
-        space
-          .getSpaceUsers()
-          .then((users) => {
-            const activatedUsers = users?.items?.filter(
-              (user) => user?.activated
-            );
-            setAuthor(activatedUsers);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
-
-    // admin
-    //   .getSpace(space_id)
-    //   .then((space) => space.getEnvironment("master-2023-04-12"))
-    //   .then((environment) => environment.getEntries())
-    //   .then((response) => {
-    //     let data = response.items;
-    //     setPublishBy(data);
-    //   })
-    //   .catch(console.error);
 
     fetchEntries();
-  }, [space_id]);
+  }, []);
 
   const handleClick = (id) => {
     navigate(`/post/${id}`);
   };
-  //console.log(articles);
 
   return (
     <div>
@@ -99,34 +67,28 @@ function Posts() {
                     style={{ height: "300px", width: "300px" }}
                   />
                   <Card.Body>
-                    <Card.Title>{art.fields.name}</Card.Title>
-                    {author &&
-                      author.slice(0, 1).map((author, i) => {
-                        return (
-                          <div
-                            key={i}
-                            className="author"
-                            style={{
-                              display: "flex",
-                              textAlign: "start",
-                            }}
-                          >
-                            <Card.Img
-                              variant="top"
-                              src={author?.avatarUrl}
-                              style={{
-                                height: "28px",
-                                width: "28px",
-                                borderRadius: "50%",
-                              }}
-                            />
-                            <h6 style={{ marginLeft: "4px", marginTop: "4px" }}>
-                              {author.firstName}
-                              <span> {dateFormat(art?.sys?.updatedAt)}</span>
-                            </h6>
-                          </div>
-                        );
-                      })}
+                    <Card.Title>{art.fields.title}</Card.Title>
+                    <div
+                      className="author"
+                      style={{
+                        display: "flex",
+                        textAlign: "start",
+                      }}
+                    >
+                      <Card.Img
+                        variant="top"
+                        src={art.fields.avtar.fields.file.url}
+                        style={{
+                          height: "35px",
+                          width: "35px",
+                          borderRadius: "50%",
+                        }}
+                      />
+                      <h6 style={{ marginLeft: "4px", marginTop: "6px" }}>
+                        {art.fields.author}
+                        <span> {dateFormat(art?.sys?.updatedAt)}</span>
+                      </h6>
+                    </div>
                   </Card.Body>
                 </Card>
               </Col>
